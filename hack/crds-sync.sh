c0eb4ee6f4ad1d179c0cc6649a2dd5c2c0c5b5a9
Richard Vanderpool <49568690+rvanderp3@users.noreply.github.com>
2023-09-20 16:55:06 -0400

    update crd sync to sync techpreview/customnoupgrade CRDs

--- hack/crds-sync.sh
+++ hack/crds-sync.sh
@@ -2,4 +2,6 @@
 
 set -euo pipefail
 
-cp "vendor/github.com/openshift/api/machine/v1/0000_10_controlplanemachineset.crd.yaml" "manifests/0000_30_control-plane-machine-set-operator_00_controlplanemachineset.crd.yaml"
+cp "vendor/github.com/openshift/api/machine/v1/0000_10_controlplanemachineset-Default.crd.yaml" "manifests/0000_30_control-plane-machine-set-operator_00_controlplanemachineset-Default.crd.yaml"
+cp "vendor/github.com/openshift/api/machine/v1/0000_10_controlplanemachineset-TechPreviewNoUpgrade.crd.yaml" "manifests/0000_30_control-plane-machine-set-operator_00_controlplanemachineset-TechPreviewNoUpgrade.crd.yaml"
+cp "vendor/github.com/openshift/api/machine/v1/0000_10_controlplanemachineset-CustomNoUpgrade.crd.yaml" "manifests/0000_30_control-plane-machine-set-operator_00_controlplanemachineset-CustomNoUpgrade.crd.yaml"
